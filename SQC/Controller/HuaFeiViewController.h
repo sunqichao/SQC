96c149b9d0d02c06a64da84d4a772a6259067af2
sunqichao <sunqichaojob@gmail.com>
2013-12-20 16:58:47 +0800

    12 yue 20 xia ban qian

---
+++ SQC/Controller/HuaFeiViewController.h
@@ -0,0 +1,13 @@
+//
+//  HuaFeiViewController.h
+//  SQC
+//
+//  Created by sun qichao on 13-12-20.
+//  Copyright (c) 2013年 sun qichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HuaFeiViewController : UIViewController
+
+@end
---
+++ SQC/Controller/HuaFeiViewController.m
@@ -0,0 +1,38 @@
+//
+//  HuaFeiViewController.m
+//  SQC
+//
+//  Created by sun qichao on 13-12-20.
+//  Copyright (c) 2013年 sun qichao. All rights reserved.
+//
+
+#import "HuaFeiViewController.h"
+
+@interface HuaFeiViewController ()
+
+@end
+
+@implementation HuaFeiViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
